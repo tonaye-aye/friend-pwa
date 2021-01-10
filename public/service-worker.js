155a44ffc9240d211e183830a1f5b609e383ccb0
Tony Gorgioski <tony.gorgioski@gmail.com>
2021-01-10 14:52:00 +1100

    copy updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,14 +6,3 @@ workbox.routing.registerRoute(
   ({ request }) => request.destination === 'image',
   new workbox.strategies.CacheFirst()
 )
-
-workbox.precaching.addPlugins([
-  new workbox.broadcastUpdate.Plugin('precache-channel')
-])
-
-const updateChannel = new BroadcastChannel('precache-channel')
-updateChannel.addEventListener('message', (event) => {
-  if (confirm(`New content is available!. Click OK to refresh`)) {
-    window.location.reload()
-  }
-})
