b6b572af6e6af398d272576785ff896af480b8b0
Tony Gorgioski <tony.gorgioski@gmail.com>
2021-01-10 14:27:31 +1100

    copy updates

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ export default function App() {
           Install app
         </Button>
         <Typography variant="h4" gutterBottom align="center" color="primary">
-          Todo list
+          Remind me
         </Typography>
         <Form
           inputText={inputText}
